a58b7dc69c5cd5e0d2c0f922674b5eb9f2bf0106
tiwari-ashish-clouddev <ashisht.clouddev@gmail.com>
2024-07-12 00:37:52 +0530

    Added User Module Skeleton, also modified Product Module.

--- product-module/src/main/java/com/assembleyourpc/app/ProductModuleApplication.java
+++ product-module/src/main/java/com/assembleyourpc/app/ProductModuleApplication.java
@@ -3,6 +3,7 @@ package com.assembleyourpc.app;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @RequestMapping
+@EnableDiscoveryClient
 public class ProductModuleApplication {
 
 	public static void main(String[] args) {
